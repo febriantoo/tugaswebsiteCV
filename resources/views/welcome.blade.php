efe02aa11702fcf69d72a7491e4e90c7d70c22c2
febriantoo <7febrianto@gmail.com>
2023-03-21 23:59:12 +0700

    first comit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+
+  <!-- My CSS -->
+  <link rel="stylesheet" href="style.css" />
+
+  <title>CV Febrianto</title>
+</head>
+<body id="Home">
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light shadow-sm" style="background-color: #dddddd" >
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Portofolio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#Skill">Skill</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- End Navbar -->
+
+<!-- Jumbotron -->
+
+<section class="jumbotron text-center">
+  <img src="img/stockvault-clear-sky212160 (1).jpg" alt="Febrianto" width="200" class="rounded-circle img-thumbnail" />
+  <h1 class="display-4">Febrianto</h1>
+  <p class="lead">Mahasiswa</p>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fill-opacity="1" d="M0,160L26.7,170.7C53.3,181,107,203,160,202.7C213.3,203,267,181,320,154.7C373.3,128,427,96,480,106.7C533.3,117,587,171,640,202.7C693.3,235,747,245,800,218.7C853.3,192,907,128,960,128C1013.3,128,1067,192,1120,229.3C1173.3,267,1227,277,1280,240C1333.3,203,1387,117,1413,74.7L1440,32L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg>
+</section>
+
+<!-- End Jumbotron-->
+
+<!-- About -->
+
+<section id="about">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col">
+        <h2>ABOUT</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col text-center">
+        <p>seorang mahasiswa dengan hoby membaca webtoon ataupun bermain game online</p>
+      </div>
+    </div>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path
+      fill="#e2edff"
+      fill-opacity="1"
+      d="M0,160L48,176C96,192,192,224,288,208C384,192,480,128,576,122.7C672,117,768,171,864,192C960,213,1056,203,1152,186.7C1248,171,1344,149,1392,138.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+    ></path>
+  </svg>
+</section>
+
+<!-- END ABOUT -->
+
+<!-- Skill -->
+
+<section id="Skill">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col">
+        <h2>Skill</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <div class="card">
+          <img src="views/img/Adobe_After_Effect_CC_icon_svg.pg" class="card-img-top" alt="After effect" />
+          <div class="card-body">
+            <p class="card-text">After Effects adalah sebuah software animasi yang digunakan untuk membuat dan menambahkan efek visual, animasi, hingga motion ke dalam sebuah video</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 mb-3">
+        <div class="card">
+          <img src="img/comingsoon.png" class="card-img-top" alt="Coding" />
+          <div class="card-body">
+            <p class="card-text">SOON</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path
+      fill="#f2f2f2"
+      fill-opacity="1"
+      d="M0,224L40,192C80,160,160,96,240,101.3C320,107,400,181,480,181.3C560,181,640,107,720,80C800,53,880,75,960,85.3C1040,96,1120,96,1200,128C1280,160,1360,224,1400,256L1440,288L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+    ></path>
+  </svg>
+</section>
+
+<!-- Skill -->
+
+<!-- Contact -->
+<section id="contact">
+  <div class="container">
+    <div class="row text-center mb-3">
+      <div class="col">
+        <h2>Contact Me</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <form>
+          <div class="mb-3">
+            <label for="Name" class="form-label">Nama Lengkap</label>
+            <input type="text" class="form-control" id="name" aria-describedby="name" />
+              <div class="mb-3">
+            <label for="Email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="Email" aria-describedby="Email" />
+            <div class="mb-3">
+                <label for="Pesan" class="form-label">Pesan</label>
+                <textarea class="form-control" id="Pesan" rows="3"></textarea>
+              </div>
+          <button type="submit" class="btn btn-primary">Kirim</button>
+        </form>
+      </div>
+    </div>
+  </div>
+ 
+</section>
+
+<!-- Contact -->
+
+
+<!-- Footer -->
+<footer class="bg-primary text-white text-center pb-5">
+    <p>My Instagram <a href="https://www.instagram.com/_mofuumoffu/" class="text-white fw-fold">Febrianto</a></p>
+</footer>
